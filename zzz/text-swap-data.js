1de50d6788f22d75af5102f19f904140b808fac8
HumanRobotFaker <antarcking@gmail.com>
2023-03-18 21:22:58 -0500

    testing reload of site

---
+++ zzz/text-swap-data.js
@@ -0,0 +1,11 @@
+const textSwapData = {
+    "atp-molecule": {
+        type: "sequenced",
+        index: 0,
+        texts: [
+            "ATP molecules",
+            "ATP compounds",
+            "adenosine triphosphate",
+        ]
+    },
+}
