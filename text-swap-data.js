fbc3082e21e4f1a9ce48ab5f0401cb5a5e0e5de5
HumanRobotFaker <antarcking@gmail.com>
2023-03-25 14:31:20 -0500

    Update text-swap-data.js

--- text-swap-data.js
+++ text-swap-data.js
@@ -7,7 +7,7 @@ const textSwapData = {
             "Ahura Mazda",
             "Ah Ur A Ma Z Da",
             "A H U R A M A Z D A",
-            document.createElement('a').innerText('texty').setAttribute('src', 'zombo.com')
+            
         ]
     },
 }
