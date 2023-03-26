89904c15148286d43bafbe97e4d98cf66c35baf1
HumanRobotFaker <antarcking@gmail.com>
2023-03-26 11:20:30 -0500

    e

---
+++ ex/js.js
@@ -0,0 +1,89 @@
+const body = document.querySelector("body")
+const popUp = document.querySelector("#popup")
+const popUpInducers = document.querySelectorAll(".popup-inducer")
+const swappingTexts = document.querySelectorAll(".text-swap")
+
+
+body.addEventListener("click", handleBodyClick)
+
+for(let inducer of popUpInducers){
+    inducer.addEventListener("click", openPopUp)
+}
+
+for(let text of swappingTexts){
+    text.addEventListener("click", swapText)
+}
+
+
+function handleBodyClick(e) {
+    const clickWasPopup =
+        e.target.classList.contains("popup") ||
+        e.target.classList.contains("popup-inducer")
+    
+    if(!clickWasPopup){
+        closePopUp()
+    }
+}
+
+function openPopUp(e) {
+    const popUpId = e.target.attributes["popup-id"].nodeValue
+    const newContent = createPopUpElement(popUpData[popUpId])
+    popUp.replaceChildren(newContent)
+    showElement(popUp)
+}
+
+function closePopUp() {
+    popUp.replaceChildren()
+    hideElement(popUp)
+}
+
+function createPopUpElement(elementData) {
+    const newThing = document.createElement(elementData.tag)
+    
+    let attributes = Object.keys(elementData.attributes)
+    for(let attribute of attributes){
+        newThing.setAttribute(attribute, elementData.attributes[attribute])
+    }
+
+    newThing.classList.add("popup")
+    for(let className of elementData.classNames){
+        newThing.classList.add(className)
+    }
+    return newThing
+}
+
+function showElement(element) {
+    element.classList.remove("hidden")
+    element.classList.add("showing")
+}
+
+function hideElement(element) {
+    element.classList.remove("showing")
+    element.classList.add("hidden")
+}
+
+function swapText(event) {
+    const outerSpan = event.currentTarget
+    const textData = textSwapData[outerSpan.id]
+
+    let index = textData.index
+    index = index + 1
+    textData.index = index
+
+    const newText = textData.texts[index % textData.texts.length]
+
+    outerSpan.replaceChildren(newText)
+}
+
+
+
+
+
+//throwaway for example thing
+
+
+
+
+
+
+
---
+++ ex/popup-data.js
@@ -0,0 +1,325 @@
+const popUpData = {
+    "hector-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Hector.png",
+        },
+        classNames: ["person-id"]
+    },
+    "loretta-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Loretta.png",
+        },
+        classNames: ["person-id"]
+    },
+    "hurley-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Hurley.png",
+        },
+        classNames: ["person-id"]
+    },
+    "suraj-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Suraj.png",
+        },
+        classNames: ["person-id"]
+    },
+    "luna-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Luna.png",
+        },
+        classNames: ["person-id"]
+    },
+    "biological-mass": {
+        tag: "img",
+        attributes: {
+            src: "./images/biologicalmass00.png",
+        },
+        classNames: ["person-id"]
+    },
+
+    "britni-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Britni.png",
+        },
+        classNames: ["person-id"]
+    },
+    "eastdaytonyeshians": {
+        tag: "img",
+        attributes: {
+            src: "./images/EastDaytonYeshians.png",
+        },
+        classNames: ["person-id"]
+
+    },
+    "andrew-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Andrew.png",
+        },
+        classNames: ["person-id"]
+    },
+    "uriah-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Uriah.png",
+        },
+        classNames: ["person-id"]
+    },
+    "me-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Xiander.png",
+        },
+        classNames: ["person-id"]
+    },
+    "jakob-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Jakob.png",
+        },
+        classNames: ["person-id"]
+    },
+    "khali-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Khali.png",
+        },
+        classNames: ["person-id"]
+    },
+    "paola-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Paola.png",
+        },
+        classNames: ["person-id"]
+    },
+    "brazil-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Brazil.png",
+        },
+        classNames: ["person-id"]
+    },
+    "tamaya-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Tamaya.png",
+        },
+        classNames: ["person-id"]
+    },
+    "luuk-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Luuk.png",
+        },
+        classNames: ["person-id"]
+    },
+    "mars-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Mars.png",
+        },
+        classNames: ["person-id"]
+    },
+    "winfield-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Winfield.png",
+        },
+        classNames: ["person-id"]
+    },
+    "jeremiah-id": {
+        tag: "img",
+        attributes: {
+            src: "./images/ID-Jeremiah.png",
+        },
+        classNames: ["person-id"]
+    },
+    "bernies-logo": {
+        tag: "img",
+        attributes: {
+            src: "./images/BerniesGroceryLogo2.png",
+        },
+        classNames: ["person-id"]
+        
+    },
+    "cupnews-logo": {
+        tag: "img",
+        attributes: {
+            src: "./images/CitizensUnitedProgramming-LogoAd.png",
+        },
+        classNames: ["person-id"]
+        
+    },
+    "tacohat": {
+        tag: "img",
+        attributes: {
+            src: "./images/TacoHatAdvertisement.png",
+        },
+        classNames: ["person-id"]
+        
+    },
+    "dietcitrusdrop-bounce": {
+        tag: "img",
+        attributes: {
+            src: "./images/Bouncing-AD.png",
+        },
+        classNames: ["person-id"]
+        
+    },
+    "sriphepl": {
+        tag: "img",
+        attributes: {
+            src: "./images/sriphepl.png",
+        },
+        classNames: ["person-id"]
+        
+    },
+    "daltondream": {
+        tag: "img",
+        attributes: {
+            src: "./images/DoctorDaltonDream.png",
+        },
+        classNames: ["person-id"]
+        
+    },
+    "dietcitrusdrop-bounce": {
+        tag: "img",
+        attributes: {
+            src: "./images/Bouncing-AD.png",
+        },
+        classNames: ["person-id"]
+        
+    },
+    "flag-wave": {
+        tag: "img",
+        attributes: {
+            src: "./images/AntarcticFlagWaving.gif",
+        },
+        classNames: ["person-id"]
+    },
+    "author-gif": {
+        tag: "img",
+        attributes: {
+            src: "./images/Author-Devolve-AI.gif",
+        },
+        classNames: ["person-id"]
+        
+
+    },
+    "doublebond": {
+        tag: "img",
+        attributes: {
+            src: "./images/DoubleBondSwitch.gif",
+        },
+        classNames: ["person-id"]
+        
+    },
+    "azüul-dream": {
+        tag: "img",
+        attributes: {
+            src: "./images/AzüulDreamPalaceScene.png",
+        },
+        classNames: ["person-id"]
+        
+    },
+    
+    "bouncing-ad": {
+        tag: "img",
+        attributes: {
+            src: "./images/Bouncing-AD.png",
+        },
+        classNames: ["person-id"]
+        
+    },
+    "yeshi-deathshrine": {
+        tag: "img",
+        attributes: {
+            src: "./images/YeshiApperance-DeathShrine.png",
+        },
+        classNames: ["person-id"]
+        
+    },
+    "yeshi-scene": {
+        tag: "img",
+        attributes: {
+            src: "./images/YeshiScene.png",
+        },
+        classNames: ["person-id"]
+    },
+    "dr-dalton-casket-ad": {
+        tag: "img",
+        attributes: {
+            src: "./images/Dr.Dalton3000GlassCasketAdvertisement.png",
+        },
+        classNames: ["person-id"]
+    },
+    "dr-dalton-casket": {
+        tag: "img",
+        attributes: {
+            src: "./images/DoctorDaltonCasket.png",
+        },
+        classNames: ["person-id"]
+    },
+    "dr-dalton-book-ad": {
+        tag: "img",
+        attributes: {
+            src: "./images/DoctorDaltonBookAdvertisement.png",
+        },
+        classNames: ["person-id"]
+    },
+    "brainstorm": {
+        tag: "img",
+        attributes: {
+            src: "https://64.media.tumblr.com/773264a3b60fe279ff05d3ca885b0af8/tumblr_pmd6e6Ol7y1runoqyo2_r2_540.gifv",
+        },
+        classNames: ["person-id"]
+    },
+    "physics": {
+        tag: "iframe",
+        attributes: {
+            width:"560",
+            height:"315",
+            src:"https://www.youtube.com/embed/hbmf0bB38h0",
+            title:"YouTube video player",
+            frameborder:"0",
+            allow:"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
+            allowfullscreen:true
+        },
+        classNames: ["person-id"]
+    },
+    "tvprophet": {
+        tag: "iframe",
+        attributes: {
+            width:"560",
+            height:"315",
+            src:"./mov/TV-Prophet001.mp4",
+            title:"GIF-to-MOV",
+            frameborder:"0",
+            allow:"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
+            allowfullscreen:true
+        },
+        classNames: ["person-id"]
+    },
+    "dreamdramadetachment": {
+        tag: "iframe",
+        attributes: {
+            width:"560",
+            height:"315",
+            src:"https://www.youtube.com/embed/1j962_zYiMY",
+            title:"YouTube video player",
+            frameborder:"0",
+            allow:"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
+            allowfullscreen:true
+        },
+        classNames: ["person-id"]
+    },
+    
+}
+
