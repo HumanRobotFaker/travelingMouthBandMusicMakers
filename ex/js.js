74d7b08bd6fd872cca5111b3b7df874ca868dbb0
HumanRobotFaker <antarcking@gmail.com>
2023-03-26 14:21:25 -0500

    as

--- ex/js.js
+++ ex/js.js
@@ -76,6 +76,20 @@ function swapText(event) {
 }
 
 
+const text = document.getElementById('text');
+const originalText = text.innerText;
+const characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', '|', ';', ':', '"', "'", ',', '.', '<', '>', '/', '?', '\\', '`', '~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+function start() {
+  let gibberishText = '';
+
+  for (let i = 0; i < originalText.length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    gibberishText += characters[randomIndex];
+  }
+
+  text.innerText = gibberishText;
+}
 
 
 
