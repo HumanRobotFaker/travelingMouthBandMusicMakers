dca011020ec7b39aba81fb3c589f26823d73fcb0
HumanRobotFaker <103977997+HumanRobotFaker@users.noreply.github.com>
2024-12-22 09:22:11 -0600

    asdf

--- 00.js
+++ 00.js
@@ -1,5 +1,6 @@
-const hoverables = document.querySelectorAll ('.hoverable');
-const IDpopUp = document.querySelector ('#IDcard');
+const hoverables = document.querySelectorAll ('.hoverable')
+const IDpopUp = document.querySelector ('#IDcard')
+const textSwapElements = document.querySelectorAll ('.textswap')
 
 const IDimages = {
 
@@ -13,25 +14,42 @@ const IDimages = {
     winfield: "images/ID-Winfield.png",
     khali: "images/ID-Khali.png",
 
+}
+
+const IDtextSwapPath = {
+
+    moments: 'seconds',
 
 }
 
+for (const textSwapElement of textSwapElements) {
+
+    textSwapElement.addEventListener ('click', handleTextSwapClick)
+
+}
 
 for (const hoverable of hoverables) {
 
-    hoverable.addEventListener ('mouseenter', handleIDenter);
-    hoverable.addEventListener ('mouseleave', handleIDleave);
+    hoverable.addEventListener ('mouseenter', handleIDenter)
+    hoverable.addEventListener ('mouseleave', handleIDleave)
 }
 
+function handleTextSwapClick (event) {
+    console.log (event)
+    const outerSpan = event.target
+    const newText = IDtextSwapPath [outerSpan.id]
+    console.log (newText)
+    outerSpan.textContent = newText
+}
 
 function handleIDenter (event) {
 
-        const imgPath = IDimages[event.target.id];
-        IDpopUp.classList.remove('hidden');
-        const img = document.createElement ('img');
-        img.src = imgPath;
-        img.classList.add ('IDimage');
-        IDpopUp.appendChild(img);
+        const imgPath = IDimages[event.target.id]
+        IDpopUp.classList.remove('hidden')
+        const img = document.createElement ('img')
+        img.src = imgPath
+        img.classList.add ('IDimage')
+        IDpopUp.appendChild(img)
 }
    
 function handleIDleave (event) {
@@ -39,8 +57,7 @@ function handleIDleave (event) {
     while (IDpopUp.lastElementChild) {
         IDpopUp.removeChild(IDpopUp.lastElementChild);
       }
-    IDpopUp.classList.add('hidden');
+    IDpopUp.classList.add('hidden')
 
 
 }
-   
